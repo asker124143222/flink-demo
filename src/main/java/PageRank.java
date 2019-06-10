1ba8575a3925aebf5cf611628f6ccbb39691bd32
asker124143222 <124143222@qq.com>
2019-06-10 17:46:53 +0800

    fix

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -15,18 +15,43 @@ import org.apache.flink.util.Collector;
 import java.util.ArrayList;
 
 /**
- * @Author: xu.dm
- * @Date: 2019/6/3 20:10
- * @Description:
+ * A basic implementation of the Page Rank algorithm using a bulk iteration.
+ *
+ * <p>This implementation requires a set of pages and a set of directed links as input and works as follows. <br>
+ * In each iteration, the rank of every page is evenly distributed to all pages it points to.
+ * Each page collects the partial ranks of all pages that point to it, sums them up, and applies a dampening factor to the sum.
+ * The result is the new rank of the page. A new iteration is started with the new ranks of all pages.
+ * This implementation terminates after a fixed number of iterations.<br>
+ * This is the Wikipedia entry for the <a href="http://en.wikipedia.org/wiki/Page_rank">Page Rank algorithm</a>.
+ *
+ * <p>Input files are plain text files and must be formatted as follows:
+ * <ul>
+ * <li>Pages represented as an (long) ID separated by new-line characters.<br>
+ * For example <code>"1\n2\n12\n42\n63"</code> gives five pages with IDs 1, 2, 12, 42, and 63.
+ * <li>Links are represented as pairs of page IDs which are separated by space
+ * characters. Links are separated by new-line characters.<br>
+ * For example <code>"1 2\n2 12\n1 12\n42 63"</code> gives four (directed) links (1)-&gt;(2), (2)-&gt;(12), (1)-&gt;(12), and (42)-&gt;(63).<br>
+ * For this simple implementation it is required that each page has at least one incoming and one outgoing link (a page can point to itself).
+ * </ul>
+ *
+ * <p>Usage: <code>PageRankBasic --pages &lt;path&gt; --links &lt;path&gt; --output &lt;path&gt; --numPages &lt;n&gt; --iterations &lt;n&gt;</code><br>
+ * If no parameters are provided, the program is run with default data from {PageRankData} and 10 iterations.
+ *
+ * <p>This example shows how to use:
+ * <ul>
+ * <li>Bulk Iterations
+ * <li>Default Join
+ * <li>Configure user-defined functions using constructor parameters.
+ * </ul>
  */
 public class PageRank {
     //阻尼系数
     private static final double DAMPENING_FACTOR = 0.85;
-    //Rúguǒ diédài zhī jiān de cānshù zhī hé dī yú cǐ, wǒmen jiāng huì shōuliǎn.
+    //收敛阈值.
     private static final double EPSILON = 0.0001;
 
-    private static DataSet<Long> getPagesDataSet(ExecutionEnvironment env, ParameterTool params){
-        if(params.has("pages")){
+    private static DataSet<Long> getPagesDataSet(ExecutionEnvironment env, ParameterTool params) {
+        if (params.has("pages")) {
             return env.readCsvFile(params.get("params"))
                     .fieldDelimiter(" ")
                     .lineDelimiter("\n")
@@ -37,29 +62,27 @@ public class PageRank {
                             return value.f0;
                         }
                     });
-        }
-        else{
+        } else {
             System.out.println("Executing PageRank example with default pages data set.");
             System.out.println("Use --pages to specify file input.");
             return PageRankData.getDefaultPagesDataSet(env);
         }
     }
 
-    private static DataSet<Tuple2<Long,Long>> getLinksDataSet(ExecutionEnvironment env,ParameterTool params){
-        if(params.has("links")){
+    private static DataSet<Tuple2<Long, Long>> getLinksDataSet(ExecutionEnvironment env, ParameterTool params) {
+        if (params.has("links")) {
             return env.readCsvFile(params.get("links"))
-                   .fieldDelimiter(" ")
-                   .lineDelimiter("\n")
-                   .types(Long.class,Long.class);
-        }
-        else{
+                    .fieldDelimiter(" ")
+                    .lineDelimiter("\n")
+                    .types(Long.class, Long.class);
+        } else {
             System.out.println("Executing PageRank example with default links data set.");
             System.out.println("Use --links to specify file input.");
             return PageRankData.getDefaultEdgeDataSet(env);
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         ParameterTool params = ParameterTool.fromArgs(args);
         final int numPages = params.getInt("numPages", PageRankData.getNumberOfPages());
         final int maxIterations = params.getInt("iterations", 10);
@@ -71,29 +94,30 @@ public class PageRank {
         env.getConfig().setGlobalJobParameters(params);
 
         // get input data
-        DataSet<Long> pagesInput = getPagesDataSet(env,params);
-        DataSet<Tuple2<Long,Long>> linksInput = getLinksDataSet(env,params);
+        DataSet<Long> pagesInput = getPagesDataSet(env, params);
+        DataSet<Tuple2<Long, Long>> linksInput = getLinksDataSet(env, params);
 
         // assign initial rank to pages
-        DataSet<Tuple2<Long,Double>> pagesWithRanks = pagesInput
-                .map(new RankAssigner(1.0d/numPages));
+        DataSet<Tuple2<Long, Double>> pagesWithRanks = pagesInput
+                .map(new RankAssigner(1.0d / numPages));
 
         // build adjacency list from link input
-        DataSet<Tuple2<Long,Long[]>> adjacencyListInput = linksInput
+        DataSet<Tuple2<Long, Long[]>> adjacencyListInput = linksInput
                 .groupBy(0)
                 .reduceGroup(new BuildOutgoingEdgeList());
 
         // set iterative data set
-        IterativeDataSet<Tuple2<Long,Double>> iteration = pagesWithRanks.iterate(maxIterations);
+        IterativeDataSet<Tuple2<Long, Double>> iteration = pagesWithRanks.iterate(maxIterations);
 
-        DataSet<Tuple2<Long,Double>> newRanks = iteration.join(adjacencyListInput)
+        DataSet<Tuple2<Long, Double>> newRanks = iteration.join(adjacencyListInput)
                 .where(0).equalTo(0)
+                //迭代算子
                 .flatMap(new JoinVertexWithEdgesMatch())
                 // collect and sum ranks
                 .groupBy(0)
-                .aggregate(Aggregations.SUM,1)
+                .aggregate(Aggregations.SUM, 1)
                 // apply dampening factor
-                .map(new Dampener(PageRank.DAMPENING_FACTOR,numPages));
+                .map(new Dampener(PageRank.DAMPENING_FACTOR, numPages));
 
 
         DataSet<Tuple2<Long, Double>> finalPageRanks = iteration.closeWith(
@@ -116,11 +140,11 @@ public class PageRank {
     /**
      * A map function that assigns an initial rank to all pages.
      */
-    public static final class RankAssigner implements MapFunction<Long,Tuple2<Long,Double>>{
-        Tuple2<Long,Double> outPageWithRank;
+    public static final class RankAssigner implements MapFunction<Long, Tuple2<Long, Double>> {
+        Tuple2<Long, Double> outPageWithRank;
 
-        public RankAssigner(double rank){
-            this.outPageWithRank = new Tuple2<>(-1L,rank);
+        public RankAssigner(double rank) {
+            this.outPageWithRank = new Tuple2<>(-1L, rank);
         }
 
         @Override
@@ -149,7 +173,7 @@ public class PageRank {
             for (Tuple2<Long, Long> n : values) {
                 id = n.f0;
                 neighbors.add(n.f1);
-                System.out.println("id: "+id+" ,neighbors: "+n.f1);
+                System.out.println("id: " + id + " ,neighbors: " + n.f1);
             }
             out.collect(new Tuple2<Long, Long[]>(id, neighbors.toArray(new Long[neighbors.size()])));
         }
@@ -157,19 +181,20 @@ public class PageRank {
 
     /**
      * Join function that distributes a fraction of a vertex's rank to all neighbors.
-     * 按照页面id以及其连接页面的长度，重新计算连接点的rank
+     * 按照页面id以及其连接页面的数量，重新计算相邻点的rank，迭代10次
+     * rankToDistribute就是计算了顶点（id）指向边（neighbors）的贡献值，neighbors最终的rank值需要合计后才能算出
      */
     public static final class JoinVertexWithEdgesMatch implements FlatMapFunction<Tuple2<Tuple2<Long, Double>, Tuple2<Long, Long[]>>, Tuple2<Long, Double>> {
 
         @Override
-        public void flatMap(Tuple2<Tuple2<Long, Double>, Tuple2<Long, Long[]>> value, Collector<Tuple2<Long, Double>> out){
-            System.out.println("before:"+value);
+        public void flatMap(Tuple2<Tuple2<Long, Double>, Tuple2<Long, Long[]>> value, Collector<Tuple2<Long, Double>> out) {
+            System.out.println("before:" + value);
             Long[] neighbors = value.f1.f1;
             double rank = value.f0.f1;
             double rankToDistribute = rank / ((double) neighbors.length);
 
-            for (Long neighbor: neighbors) {
-                System.out.println("neighbor:"+neighbor+",rankToDistribute:"+rankToDistribute);
+            for (Long neighbor : neighbors) {
+//                System.out.println("neighbor:" + neighbor + ",rankToDistribute:" + rankToDistribute);
                 out.collect(new Tuple2<Long, Double>(neighbor, rankToDistribute));
             }
         }
@@ -183,7 +208,6 @@ public class PageRank {
      * C(Ti)是页面Ti的出度，也就是Ti指向其他页面的边的个数
      * d 为阻尼系数，其意义是，在任意时刻，用户到达某页面后并继续向后浏览的概率，
      * 该数值是根据上网者使用浏览器书签的平均频率估算而得，通常d=0.85
-     *
      */
     @FunctionAnnotation.ForwardedFields("0")
     public static final class Dampener implements MapFunction<Tuple2<Long, Double>, Tuple2<Long, Double>> {
@@ -206,11 +230,13 @@ public class PageRank {
     /**
      * Filter that filters vertices where the rank difference is below a threshold.
      * 如果迭代之间的参数之和低于此EPSILON，我们将会收敛
+     * 每次迭代后都会调用Filter判断是否要退出迭代
      */
     public static final class EpsilonFilter implements FilterFunction<Tuple2<Tuple2<Long, Double>, Tuple2<Long, Double>>> {
 
         @Override
         public boolean filter(Tuple2<Tuple2<Long, Double>, Tuple2<Long, Double>> value) {
+            System.out.println("value:"+value+",math:"+Math.abs(value.f0.f1 - value.f1.f1));
             return Math.abs(value.f0.f1 - value.f1.f1) > EPSILON;
         }
     }
