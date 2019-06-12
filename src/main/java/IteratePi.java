2d484428a40f265e8fe1bdef441077308910cd1b
asker124143222 <124143222@qq.com>
2019-06-12 20:57:40 +0800

    Iteration

---
+++ src/main/java/IteratePi.java
@@ -0,0 +1,53 @@
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.IterativeDataSet;
+
+/**
+ * @Author: xu.dm
+ * @Date: 2019/6/12 20:22
+ * @Description:
+ */
+public class IteratePi {
+    public static void main(String[] args) throws Exception{
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // Create initialIterativeDataSet
+        IterativeDataSet<Integer> inital = env.fromElements(0).iterate(100);
+
+        DataSet<Integer> iteration = inital.map(new MapFunction<Integer, Integer>() {
+            @Override
+            public Integer map(Integer value) throws Exception {
+                double x = Math.random();
+                double y = Math.random();
+
+                Integer result = value + ((x * x + y * y < 1) ? 1 : 0);
+
+                System.out.println("iteration:"+result);
+                return result;
+            }
+        });
+
+        // Iteratively transform the IterativeDataSet
+        DataSet<Integer> count = inital.closeWith(iteration,iteration.filter(new FilterFunction<Integer>() {
+            @Override
+            public boolean filter(Integer value) throws Exception {
+                return value<50?true:false;
+            }
+        }));
+
+//        System.out.println("count:");
+//        count.print();
+
+        count.map(new MapFunction<Integer, Double>() {
+            @Override
+            public Double map(Integer value) throws Exception {
+                Double result = value /(double) 10000 * 4;
+                return result;
+            }
+        }).print();
+
+    }
+
+}
